140475e06789a8b48396cf26ac2d5d3121acaf14
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2017-11-20 09:50:31 +0100

    #! init ima-clientify

---
+++ README.md
@@ -0,0 +1,7 @@
+# ima-clientify
+
+This is the ima library of the IMA.js application stack, which is most likely
+not what you are looking for.
+
+You can find the IMA.js skeleton application at
+<https://github.com/seznam/IMA.js-skeleton>.
---
+++ index.js
@@ -0,0 +1,67 @@
+const { Transform } = require('stream');
+
+const SERVER_SIDE = '@server-side';
+const VARIABLE = '__VARIABLE__';
+const CLEAR = '__CLEAR__';
+
+class Clientify extends Transform {
+  constructor(filename, options) {
+    super(options);
+    this._data = '';
+    this._filename = filename;
+
+    this._server = new RegExp(`${SERVER_SIDE}(.*)\n`, 'g');
+  }
+
+  _transform(buf, enc, callback) {
+    this._data += buf;
+    callback();
+  }
+
+  _flush(callback) {
+    try {
+      if (this._server.test(this._data)) {
+        let rules = this._data.match(this._server);
+
+        rules.forEach(rule => {
+          this._data = this._data.replace(rule, '');
+
+          let regPattern = rule
+            .replace(`${SERVER_SIDE} `, '')
+            .replace('\n', '');
+
+          let variablePattern = regPattern
+            .replace(VARIABLE, '(.*)')
+            .replace(CLEAR, '(?:(?:.|\n)*)');
+
+          const variables = this._data.match(variablePattern);
+          let variableKey = 0;
+
+          let clearablePattern = regPattern
+            .replace(VARIABLE, () => {
+              variableKey += 1;
+
+              return variables[variableKey];
+            })
+            .replace(CLEAR, '((.|\n)*)');
+
+          let clearingParts = this._data.match(clearablePattern);
+
+          if (clearingParts) {
+            clearingParts.slice(1).forEach(part => {
+              this._data = this._data.replace(part, '');
+            });
+          }
+        });
+      }
+
+      this.push(this._data);
+    } catch (err) {
+      this.emit('error', err);
+      return;
+    }
+    callback();
+  }
+}
+
+module.exports = (...rest) => new Clientify(...rest);
